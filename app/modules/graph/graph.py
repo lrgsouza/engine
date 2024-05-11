71e2b9232a9736d48fb8caf33f6910af5887ceb8
lrgsouza <lucas_souza@outlook.com>
2024-05-11 01:40:49 -0300

    improve graph class

--- app/modules/graph/graph.py
+++ app/modules/graph/graph.py
@@ -8,33 +8,43 @@ class Graph:
         self.b = b
         self.c = c
 
-    def generate_first_degree_graph(self):
+    def generate_graph_legend(self):
+        if self.a == 0 and self.b == 0 and self.c == 0:
+            return "y = 0", "primeiro"
+        if self.a == 0 and self.b == 0:
+            return f"y = {self.c}", "primeiro"
+        if self.a == 0 and self.c == 0:
+            return f"y = {self.b}x", "primeiro"
+        if self.b == 0 and self.c == 0:
+            return f"y = {self.a}x²", "segundo"
+        if self.a == 0:
+            return f"y = {self.b}x + {self.c}", "primeiro"
+        if self.b == 0:
+            return f"y = {self.a}x² + {self.c}", "segundo"
+        if self.c == 0:
+            return f"y = {self.a}x² + {self.b}x", "segundo"
+        return f"y = {self.a}x² + {self.b}x + {self.c}" , "segundo"
 
-        x = np.linspace(-10, 10, 400)
-        y = self.a*x + self.b
-
-        plt.figure()
-        plt.plot(x, y)
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.grid(True)
-        plt.title(f'Graph of y = {self.a}x + {self.b}')
-        plt.axhline(0, color='black',linewidth=0.5)
-        plt.axvline(0, color='black',linewidth=0.5)
-        plt.savefig('grafico_funcao.png')
-
-    def generate_second_degree_graph(self):
+    def generate_graph(self):
         x = np.linspace(-10, 10, 400)
         
         # Calcula os valores correspondentes para o eixo y usando a função quadrática
         y = self.a * x**2 + self.b * x + self.c
-        
+
+        legend, degree = self.generate_graph_legend()
+        # apaga o arquivo se existir
+        if os.path.isfile(f'grafico_funcao_{degree}_grau.png'):
+            os.remove(f'grafico_funcao_{degree}_grau.png')
+            
         # Cria o gráfico
-        plt.plot(x, y, label=f'{self.a}x^2 + {self.b}x + {self.c}')
+        plt.figure()
+        plt.plot(x, y)
         plt.xlabel('x')
         plt.ylabel('y')
-        plt.title(f'Gráfico da função de segundo grau: y = {self.a}x² + {self.b}x + {self.c}')
+        plt.title(f'Graph of the function: {legend}')
         plt.grid(True)
         plt.axhline(0, color='black',linewidth=0.5)
         plt.axvline(0, color='black',linewidth=0.5)
-        plt.savefig('grafico_funcao_segundo_grau.png')
+        plt.savefig(f'grafico_funcao_{degree}_grau.png')
+
+        return legend, degree
