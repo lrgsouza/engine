a09fdcd7724c66aefc9b73a947c9ee2c34adca37
airtong <airton-carvalho@outlook.com>
2024-05-11 23:27:45 -0300

    [refactor] separate main file

--- app/main.py
+++ app/main.py
@@ -1,114 +1,17 @@
 import os
-import discord
-from loguru import logger
-from discord.ext import commands
-import datetime
-import pytz
-from discord import Embed, option
+from discord import option
 
 from modules.dice.dice import Dice
 from modules.graph.graph import Graph
-# Variables
-channel_name = os.getenv('CHANNEL_NAME', "recursos")
-token_discord = os.getenv('DISCORD_TOKEN', "")
-brazil = pytz.timezone("America/Sao_Paulo")
-rule_id = os.getenv('RULE_ID')
-guild_id = os.getenv('GUILD_ID', "1233550515569295401")
+from modules.currency.currency import Currency
+from variables import *
+from discord_function import *
 
-intents = discord.Intents.all()
-intents.message_content = True
-
-# bot prefix
-bot = commands.Bot(command_prefix='/', help_command=None, intents=intents)
-
-# Variables messages
-denied_msg = "You dont have permission to do that!"
-wrong_msg = ":warning: Something wrong, try use `/help`"
-timeout_msg = "Timeout, please try again!"
-
-##############################
-###### CONFIG FUNCTIONS ######
-##############################
-
-@bot.event
-async def on_ready():
-    logger.info(f"{bot.user.name} has connected to Discord!")
-
-async def exit(ctx):
-    await ctx.respond("tchau")
-
-def is_in_channel():
-    def predicate(ctx):
-        if ctx.channel.name != channel_name:
-            raise commands.CheckFailure(f"You must be in the '{channel_name}' channel to use this command.")
-        else:
-            return True
-    return commands.check(predicate)
-
-# Verify if the user is adm or has the role
-def is_adm_or_has_role(role=None):
-    async def predicate(ctx):
-        try:
-            is_adm = await commands.has_permissions(administrator=True).predicate(ctx)
-            if is_adm:
-                return True
-        except commands.CheckFailure:
-            pass
-
-        if role is None:
-            raise commands.CheckFailure("Only administrators can use this command.")
-        else:
-            try:
-                has_role = await commands.has_any_role(role).predicate(ctx)
-                if has_role:
-                    return True
-            except commands.CheckFailure:
-                raise commands.CheckFailure(f"You need the '{role}' role to use this command.")
-
-    return commands.check(predicate)
-
-
-
-# Função para enviar a imagem para o canal Discord
 async def send_graph(ctx, file):
     with open(file, 'rb') as f:
         file = discord.File(f)
         await ctx.send(file=file)
 
-
-##############################
-###### DISCORD COMMANDS ######
-##############################
-
-@bot.slash_command(name="help", description="Show help information", pass_context=True, guild_ids=[guild_id])
-@is_in_channel()
-async def help(ctx):
-    await ctx.defer()
-    bot_message = {'message' : f"{help_msg}"'', 'color': 0xFFFF00}#yellow\
-    bot_message_formatted = Embed(title=f':bookmark_tabs: Allowed commands:', #TODO mudar url
-                        description=bot_message['message'], color=bot_message['color'], timestamp=datetime.datetime.now(brazil))
-    await ctx.respond(embed=bot_message_formatted)
-
-# Function addrole
-@bot.slash_command(name="addrole", description="Add role to user", pass_context=True, guild_ids=[guild_id])
-@option("user", discord.Member, description="User who will receive the role.")
-@option("role", discord.Role, description="Role that will be applied to the user.")
-@is_adm_or_has_role()
-@is_in_channel()
-async def addrole(ctx, user: discord.Member, role: discord.Role):
-    await ctx.defer()
-    try:
-        await user.add_roles(role)
-        await ctx.respond(f"Hey, {user.mention}, you have been given the role: `{role.name}`")
-    except Exception as error:
-        logger.error(error)
-        await ctx.respond(f":x:{error}")
-
-##############################
-######### COMMANDS ###########
-##############################
-
-# Function get-pods
 @bot.slash_command(name="somar", description="Show all production instances state", pass_context=True, guild_ids=[guild_id])
 @option("number1", int, description="Number.",choices=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 @option("number2", int, description="Number.",choices=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
@@ -151,7 +54,6 @@ async def graph(ctx):
     await ctx.respond(f'Seu dado foi {result}')
 
 
-from modules.currency.currency import Currency
 @bot.slash_command(name="currency", description="Get currency value", pass_context=True, guild_ids=[guild_id])
 @option("origin", str, description="Currency code.", choices=Currency.codes())
 @option("target", str, description="Currency code.", choices=Currency.codes())
@@ -169,6 +71,7 @@ async def currency(ctx, origin: str, target: str):
         await ctx.respond(f'An error occurred while trying to get the currency value. Please try again later.')
         return
     
-    await ctx.respond(f'# {Currency.emojis()[origin]} {origin} = **{value}** {target} {Currency.emojis()[target]}')
+    formatted_value = "{:.8f}".format(value).rstrip('0').rstrip('.')
+    await ctx.respond(f'# {Currency.emojis()[origin]} {origin} = **{formatted_value}** {target} {Currency.emojis()[target]}')
 
 bot.run(token_discord)
---
+++ app/variables.py
@@ -0,0 +1,30 @@
+import os
+import pytz
+
+from discord.ext import commands
+
+import discord
+
+# Variables
+channel_name = os.getenv('CHANNEL_NAME', "test-002")
+token_discord = os.getenv('DISCORD_TOKEN', "")
+brazil = pytz.timezone("America/Sao_Paulo")
+rule_id = os.getenv('RULE_ID')
+guild_id = os.getenv('GUILD_ID', "1233550515569295401")
+
+# Variables messages
+denied_msg = "You dont have permission to do that!"
+wrong_msg = ":warning: Something wrong, try use `/help`"
+timeout_msg = "Timeout, please try again!"
+
+help_msg = """
+/help - Show help information
+/dice - Roll a dice
+/graph - Generate a graph
+/currency - Get currency value
+/exit - Exit the bot
+"""
+
+intents = discord.Intents.all()
+intents.message_content = True
+bot = commands.Bot(command_prefix='/', help_command=None, intents=intents)
