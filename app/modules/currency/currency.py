775f6139b56d295efd8b88b26de0428f9a86d938
airtong <airton-carvalho@outlook.com>
2024-05-12 00:00:07 -0300

    [feature] fix crurrency logic

--- app/modules/currency/currency.py
+++ app/modules/currency/currency.py
@@ -30,8 +30,6 @@ class Currency:
             "ETH": ":small_blue_diamond:"   # Ethereum
         }
 
-        
-        
     @staticmethod
     def currency(moeda_origem, moeda_destino):
         """
@@ -44,35 +42,36 @@ class Currency:
         Returns:
             float: Exchange rate from source to destination currency.
         """
-        # Endpoint URL
+
+        if moeda_origem == moeda_destino:
+            return 1
+
+        # --- Try to get data from API
+
         url = "https://economia.awesomeapi.com.br/last/{}-{}"
 
-        # Making the GET request
-        response = requests.get(url.format(moeda_origem, moeda_destino))
+        formatted_url = url.format(moeda_origem, moeda_destino)
 
-        # Checking if the request was successful
+        response = requests.get(formatted_url)
+
         if response.status_code == 200:
-            return float(response.json()[f"{moeda_origem}{moeda_destino}"]["bid"])
+            return float(response.json()["{}{}".format(moeda_origem, moeda_destino)]["bid"])
+
+        # --- Try to get reverse data from API
         
-        elif moeda_origem == "USD" or moeda_destino == "USD":
-            aux = moeda_destino
-            moeda_destino = moeda_origem
-            moeda_origem = aux
+        formatted_url = url.format(moeda_destino, moeda_origem)
 
-            return 1/float(requests.get(url.format(moeda_origem, moeda_destino)).json()[f"{moeda_origem}USD"]["bid"])
+        response = requests.get(formatted_url)
 
-        elif response.status_code == 404:
-            # If the specific exchange rate is not found, we try to calculate using USD as intermediate currency.
-            
-            # Exchange rate from source to USD
-            source_to_usd = float(requests.get(url.format(moeda_origem, "USD")).json()[f"{moeda_origem}USD"]["bid"])
+        if response.status_code == 200:
+            return 1 / float(response.json()["{}{}".format(moeda_destino, moeda_origem)]["bid"])
 
-            # Exchange rate from USD to destination
-            usd_to_destiny = float(requests.get(url.format("USD", moeda_destino)).json()[f"USD{moeda_destino}"]["bid"])
+        # --- Try to get it through USD
 
-            # Conversion
-            return source_to_usd * usd_to_destiny
-        else:
-            # If the request fails, print the status code
-            print("Failed to retrieve exchange rate. Status code:", response.status_code)
+        try:
+            origin_usd = Currency.currency(moeda_origem, "USD")
+            dest_usd = Currency.currency("USD", moeda_destino)
+
+            return origin_usd * dest_usd
+        except:
             return None
